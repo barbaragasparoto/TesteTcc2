eb60f7177d89f93d1e3ccbcdc052d4bff078fb87
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-08-17 12:31:24 -0300

    tela inicial

--- src/componentes/Login/index.tsx
+++ src/componentes/Login/index.tsx
@@ -1,16 +1,51 @@
-import React from 'react';
-import {Image, StyleSheet} from 'react-native'
+import React, { useState } from 'react';
+import {Dimensions, Image, StyleSheet, View, TextInput, Text} from 'react-native'
 import logo from '../../../assets/logo.png'
 
-export default function Login(){
+const width = Dimensions.get('screen').width;
 
-return <Image source={ logo } style={estilos.logo}/>
+
+export default function Login(){
+    const [text, setText] = useState('');
+
+return <View style={estilos.container}>
+    
+<Image source={ logo } style={estilos.logo}/>
+
+<TextInput
+        style={estilos.input}
+        onChangeText={setText}
+        placeholder="Digite algo"
+      />
+      <Text style={estilos.texto}>{text}</Text>
+
+</View>
 }
 
 const estilos = StyleSheet.create({
 
+    container: {
+        backgroundColor: 'black',
+        height: '100%'
+},
+
     logo: {
-     width: "100%" 
+        width: "90%",
+        height: 420 / 594 * width,
+        left: "8%"
+    },
+  
+    input : {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        padding: 10,
+        color: 'white'
+    },
+    texto: {
+        color: "white"
     }
-    
+
+
     })
